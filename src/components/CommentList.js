1f7c086c554a129fb7ba3389cbcf3498096b0225
ALSRUD29 <dev.mnkg@gmail.com>
2023-01-17 20:59:36 +0900

    feat : comment 수정

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { deleteComment } from "../store/comment";
+import { deleteComment, updateComment } from "../store/comment";
 
 // 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
 
@@ -9,16 +9,46 @@ function CommentList() {
   const comments = useSelector((store) => store.comment);
   //console.log("test", test);
   const dispatch = useDispatch();
+  const [updateMode, setUpdateMode] = useState(false);
+  const [newContentInput, setNewContentInput] = useState("");
 
-  return comments.map((comment, key) => (
+  return comments.comments.map((comment, key) => (
     <Comment key={key}>
       <img src={comment.profile_url} alt="" />
       {comment.author}
       <CreatedAt>{comment.createdAt}</CreatedAt>
-      <Content>{comment.content}</Content>
+      <Content>
+        {updateMode ? (
+          <input
+            // value={comment.content}
+            onChange={(e) => setNewContentInput(e.target.value)}
+          />
+        ) : (
+          comment.content
+        )}
+      </Content>
 
       <Button>
-        <span>수정</span>
+        {updateMode ? (
+          <span
+            onClick={() => {
+              //업데이트 진행
+              console.log(updateComment(comment.id, newContentInput));
+              dispatch(updateComment(comment.id, newContentInput));
+              setUpdateMode(false);
+            }}
+          >
+            확인
+          </span>
+        ) : (
+          <span
+            onClick={() => {
+              setUpdateMode(true);
+            }}
+          >
+            수정
+          </span>
+        )}
         <span
           onClick={() => {
             dispatch(deleteComment(comment.id));
--- src/store/comment/index.js
+++ src/store/comment/index.js
@@ -3,6 +3,7 @@
 //crud 필요
 const CREATE_COMMENT = "comments/CREATE_COMMENT";
 const DELETE_COMMENT = "comments/DELETE_COMMENT";
+const UPDATE_COMMENT = "comments/UPDATE_COMMENT";
 
 //액션 크리에이터 생성
 let nextId = 2;
@@ -19,17 +20,37 @@ const createComment = (profile_url, author, content, createdAt) => ({
 
 const deleteComment = (id) => ({ type: DELETE_COMMENT, id });
 
+const updateComment = (id, newContent) => ({
+  type: UPDATE_COMMENT,
+  payload: {
+    id,
+    content: newContent,
+  },
+});
+
 //초기상태 선언
 
-const initialState = [
-  {
-    id: 1,
-    profile_url: "https://picsum.photos/id/1/50/50",
-    author: "예시작성자",
-    content: "예시내용",
-    createdAt: "예시날짜",
-  },
-];
+// const initialState = [
+//   {
+//     id: 1,
+//     profile_url: "https://picsum.photos/id/1/50/50",
+//     author: "예시작성자",
+//     content: "예시내용",
+//     createdAt: "예시날짜",
+//   },
+// ];
+
+const initialState = {
+  comments: [
+    {
+      id: 1,
+      profile_url: "https://picsum.photos/id/1/50/50",
+      author: "abc_1",
+      content: "UI 테스트는 어떻게 진행하나요",
+      createdAt: "2022-03-01",
+    },
+  ],
+};
 
 // const initialState = getComments();
 
@@ -38,12 +59,23 @@ const commentReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_COMMENT:
       return state.concat(action.comment);
+
     case DELETE_COMMENT:
       return [...state.filter((comment) => comment.id !== action.id)];
+
+    case UPDATE_COMMENT:
+      return {
+        ...state,
+        comments: state.comments.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, content: action.payload.content }
+            : item
+        ),
+      };
     default:
       return state;
   }
 };
 
-export { createComment, deleteComment };
+export { createComment, deleteComment, updateComment };
 export default commentReducer;
